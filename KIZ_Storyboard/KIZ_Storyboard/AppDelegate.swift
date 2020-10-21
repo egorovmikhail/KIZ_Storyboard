218d794a43e5338898927b040e9599ec9ebffd52
Михаил <40540053+egorovmikhail@users.noreply.github.com>
2020-10-21 23:55:49 +1100

    Add files via upload

---
+++ KIZ_Storyboard/KIZ_Storyboard/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  KIZ_Storyboard
+//
+//  Created by Михаил Егоров on 21.10.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+}
+
---
+++ KIZ_Storyboard/KIZ_Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KIZ_Storyboard
+//
+//  Created by Михаил Егоров on 21.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
